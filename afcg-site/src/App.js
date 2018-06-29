b847050f5c3480b3cc8e9947be8279f018da7c57
Fedor <arbuzovFP@gmail.com>
2018-06-29 08:54:31 +0400

    fix

--- afcg-site/src/App.js
+++ afcg-site/src/App.js
@@ -9,6 +9,27 @@ export default function App() {
     return (
         <BrowserRouter>
             <div>
+                <nav className="navbar navbar-default navbar-fixed-top">
+                    <div className="container">
+                        <div className="navbar-header">
+                            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                            <a className="navbar-brand" href="#myPage">AFCG</a>
+                        </div>
+                        <div className="collapse navbar-collapse" id="myNavbar">
+                            <ul className="nav navbar-nav navbar-right">
+                                <li><Link to="#about">ABOUT</Link></li>
+                                <li><Link to="#portfolio">PORTFOLIO</Link></li>
+                                <li><Link to="#customers">CUSTOMERS</Link></li>
+                                <li><Link to="#contact">CONTACT</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+
                 <Switch>
                     <Route
                         path="/"
